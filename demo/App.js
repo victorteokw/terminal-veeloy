27eccb6441345f249e2846c363709814598bab13
Zhang Kaiyu <yeannylam@gmail.com>
2019-03-08 16:55:12 +0800

    dynamic icons

--- demo/App.js
+++ demo/App.js
@@ -7,16 +7,25 @@ import ButtonGroup from '../lib/ButtonGroup';
 import Button from '../lib/Button';
 import ButtonSpacer from '../lib/ButtonSpacer';
 import Heading from '../lib/Heading';
+import CloseIcon from '../lib/CloseIcon';
+import MinimizeIcon from '../lib/MinimizeIcon';
+import MaximizeIcon from '../lib/MaximizeIcon';
 
 const App = () => <Terminal>
   <TitleBar>
     <ButtonGroup>
       <ButtonSpacer />
-      <Button close />
+      <Button close>
+        <CloseIcon />
+      </Button>
       <ButtonSpacer />
-      <Button minimize />
+      <Button minimize>
+        <MinimizeIcon />
+      </Button>
       <ButtonSpacer />
-      <Button maximize />
+      <Button maximize>
+        <MaximizeIcon />
+      </Button>
       <ButtonSpacer />
     </ButtonGroup>
     <Heading>Terminal</Heading>
