3bc944edf93d48860d436d32db7a8efcbd743634
Zhang Kaiyu <yeannylam@gmail.com>
2019-03-08 14:39:19 +0800

    static shape of terminal

--- demo/App.js
+++ demo/App.js
@@ -3,9 +3,24 @@ import React from 'react';
 
 import Terminal from '../lib/Terminal';
 import TitleBar from '../lib/TitleBar';
+import ButtonGroup from '../lib/ButtonGroup';
+import Button from '../lib/Button';
+import ButtonSpacer from '../lib/ButtonSpacer';
+import Heading from '../lib/Heading';
 
 const App = () => <Terminal>
-  <TitleBar title="some title" />
+  <TitleBar>
+    <ButtonGroup>
+      <ButtonSpacer />
+      <Button close />
+      <ButtonSpacer />
+      <Button minimize />
+      <ButtonSpacer />
+      <Button maximize />
+      <ButtonSpacer />
+    </ButtonGroup>
+    <Heading>Terminal</Heading>
+  </TitleBar>
 </Terminal>;
 
 export default hot(module)(App);
