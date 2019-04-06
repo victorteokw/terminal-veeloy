ba56fb9afa2e43ead4d5b459d3b86a9b3fc67211
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-06 14:15:46 +0800

    update demo for scaffold kit

--- website/examples/ScaffoldKit.js
+++ website/examples/ScaffoldKit.js
@@ -33,9 +33,95 @@ const ScaffoldKit = <TerminalFrame darkMode>
     <Heading>Scaffold Kit Terminal</Heading>
   </TitleBar>
   <TerminalBody style={{ backgroundColor: 'black', color: 'white' }}>
-    {'$ gen-my-app'}
+    <Text>
+      <Text bold>$ </Text>
+      <Text>gen-my-server app --server=koa --orm=mongoose</Text>
+    </Text>
     <br />
-    {'love-from:terminal-veeloy dear$ '}
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>.eslintrc.json</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>.gitignore</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>README.md</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>package.json</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>routes.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>server.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>     install </Text>
+      <Text>eslint</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>     install </Text>
+      <Text>koa</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>     install </Text>
+      <Text>koa-router</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text bold>$ </Text>
+      <Text>gen-my-server model User email:String name:String age:Int</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>models/User.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      update </Text>
+      <Text>server.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text bold>$ </Text>
+      <Text>gen-my-server api users --CRUD</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      create </Text>
+      <Text>controllers/users.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      update </Text>
+      <Text>routes.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text greenBright>      update </Text>
+      <Text>server.js</Text>
+    </Text>
+    <br />
+    <Text>
+      <Text bold>$ </Text>
+      <Text>npm start</Text>
+    </Text>
     <Cursor shape='block' blink />
   </TerminalBody>
 </TerminalFrame>;
